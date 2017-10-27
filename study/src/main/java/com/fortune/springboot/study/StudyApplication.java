bd5c4a2708ef2c5217d00786bddfcacf3544df1e
wufc <Aa123456>
2017-10-27 11:35:43 +0800

    commit test

--- study/src/main/java/com/fortune/springboot/study/StudyApplication.java
+++ study/src/main/java/com/fortune/springboot/study/StudyApplication.java
@@ -3,12 +3,14 @@ package com.fortune.springboot.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 启动类
  * @author fortune.wu
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableCaching
 public class StudyApplication {
 
 	/**
