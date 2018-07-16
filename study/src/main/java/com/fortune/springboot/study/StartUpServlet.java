4ee4da2174e31e7030bbd700c3f8db214a571624
wufc <Aa123456>
2018-07-16 14:55:47 +0800

    commit test

--- study/src/main/java/com/fortune/springboot/study/StartUpServlet.java
+++ study/src/main/java/com/fortune/springboot/study/StartUpServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.ServletException;
  * 一般伴随着ServletContainerInitializer一起使用的还有HandlesTypes注解，
  * 通过HandlesTypes可以将感兴趣的一些类注入到ServletContainer Initializer的onStartup方法作为参数传入。
  *
+ * 只有用容器启动才会有用
+ *
  * @author fortune.wu
  * @date 17/10/27
  */
